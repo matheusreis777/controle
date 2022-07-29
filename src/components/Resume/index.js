acc7aaf7e5c641bbd375095493ba8622b9effcb9
matheusreis777 <matheuscronapp@gmail.com>
2022-07-29 10:20:04 -0400

    modified:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/App.js 	new file:   src/components/Header/index.js 	new file:   src/components/Header/styles.js 	new file:   src/components/Resume/index.js 	new file:   src/components/Resume/styles.js 	new file:   src/components/ResumeItem/index.js 	new file:   src/components/ResumeItem/styles.js 	new file:   src/index.js 	new file:   src/styles/global.js 	new file:   yarn.lock

---
+++ src/components/Resume/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ResumeItem from "../ResumeItem";
+import * as C from "./styles";
+
+const Resume = () => {
+    return (
+        <C.Container>
+           <ResumeItem />
+        </C.Container>
+    );
+};
+
+export default Resume;
